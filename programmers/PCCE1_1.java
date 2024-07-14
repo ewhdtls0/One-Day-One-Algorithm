689d6e1a5993dca803f5507606544c6f5da8ee96
itsjake <ewhdtls0@naver.com>
2024-07-14 23:06:28 +0900

    Create PCCE1_1.java

---
+++ programmers/PCCE1_1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String msg = 
+"Spring is beginning"
+;
+        int val1 = 
+3
+;
+        String val2 = 
+"3"
+;
+
+        System.out.println(msg);
+        System.out.println(val1 + 10);
+        System.out.println(val2 + "10");
+    }
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/250133

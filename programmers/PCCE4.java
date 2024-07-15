1f896b7532fdf1352d11138b289bacc3932c6e61
itsjake <ewhdtls0@naver.com>
2024-07-15 22:50:39 +0900

    Create PCCE4.java

---
+++ programmers/PCCE4.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int before = sc.nextInt();
+        int after = sc.nextInt();
+
+        int money = start;
+        int month = 1;
+        while (money < 70) {
+            money += 
+before
+;
+            month++;
+        }
+        while (
+money
+ < 100) {
+            
+money += after
+;
+            month++;
+        }
+
+        System.out.println(month);
+    }
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/250130
